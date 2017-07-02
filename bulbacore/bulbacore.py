cada8b44fb2070fb4888abc1120008231ae99d18
retrodpc <retrodpc@hotmail.com>
2017-07-02 19:04:00 -0400

    oops i did a double stupid

--- bulbacore/bulbacore.py
+++ bulbacore/bulbacore.py
@@ -542,14 +542,14 @@ class Bulbacore:
 
     @commands.command(pass_context=True, aliases=['megadrive'], hidden = True)
     @asyncio.coroutine
-    def genesis(self):
+    def genesis(self, ctx):
         """..."""
         if (shiptoast_check(self, ctx.message)):
             yield from self.bot.say("Why would someone initiate the genesis of such a horrid contraption?")
 
     @commands.command(pass_context=True, aliases=['love'])
     @asyncio.coroutine
-    def hyena(self):
+    def hyena(self, ctx):
         """..."""
         if (shiptoast_check(self, ctx.message)):
             yield from self.bot.say("I have never kissed a girl. I have a tendency to lie awake at night and hope that someday that changes, but that would take a miracle. I wish I could go back in time and choose a different set of hobbies, but that probably is not going to happen. If it did, I would spend more time being active. I have a malformed body. I will probably die before my time because I liked to look at television sets and computer monitors instead of admiring the natural beauty of the outdoors. I want to wake up in my bed to the sound of a girl's breathing. I want to feel her body heat at my back. That's me.")
