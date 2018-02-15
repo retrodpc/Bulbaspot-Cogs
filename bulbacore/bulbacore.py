09c34fdc960fd08bd4970beeb09a428e7f2d7e87
Ivysalt <retrodpc@hotmail.com>
2018-02-15 15:09:23 -0500

    adding shiptoast thing for cum

--- bulbacore/bulbacore.py
+++ bulbacore/bulbacore.py
@@ -665,9 +665,11 @@ class Bulbacore:
         if (message.author != self.bot.user) and (shiptoast_check(self, message)):
             if (message.content.lower().find("case in point") != -1):
                 yield from self.bot.send_message(message.channel, '\uD83D\uDC49\uD83D\uDCBC point in case')
-            elif (message.content.lower().startswith('ok')
-            and (message.channel.id != "222432649472376832")):
-                yield from self.bot.send_message(message.channel, 'ok')
+            if (message.channel.id != "222432649472376832"):
+                if ("cum" in message.content.lower().split()):
+                    yield from self.bot.send_message(message.channel, 'oi mate watch your fuckin language')
+                elif (message.content.lower().startswith('ok')):
+                    yield from self.bot.send_message(message.channel, 'ok')
 
 
 def check_folders():
