962bd77acec969958b2782d948619d39325498d2
Ivysalt <retrodpc@hotmail.com>
2018-02-15 20:00:34 -0500

    adding dm support for loglast

--- logger/logger.py
+++ logger/logger.py
@@ -184,19 +184,22 @@ class Logger:
             for recipient in ctx.message.channel.recipients:
                 recipients_list += "{}#{}".format(recipient.name, recipient.discriminator)
             filename = 'DM-({6})-{0:04d}{1:02d}{2:02d}-{3:02d}-{4:02d}-{5:02d}.log'.format(datetime.now().year, datetime.now().month, datetime.now().day, datetime.now().hour, datetime.now().minute, datetime.now().second, ",".join(recipients_list))
+            intro = "Log started by {6} on {0:04d}/{1:02d}/{2:02d} at {3:02d}:{4:02d}:{5:02d} with recipient {7}.\nUser ID: {8}\nChannel ID: {9}\nChannel type: {10}\n".format(datetime.now().year, datetime.now().month, datetime.now().day, datetime.now().hour, datetime.now().minute, datetime.now().second, ctx.message.author.name, ", ".join(recipients_list), ctx.message.author.id, ctx.message.channel.id, ctx.message.channel.type)
         elif (str(ctx.message.channel.type) == "group"):
             recipients_list = []
             for recipient in ctx.message.channel.recipients:
                 recipients_list += "{}#{}".format(recipient.name, recipient.discriminator)
             filename = 'Group-{6}-({7})-{0:04d}{1:02d}{2:02d}-{3:02d}-{4:02d}-{5:02d}.log'.format(datetime.now().year, datetime.now().month, datetime.now().day, datetime.now().hour, datetime.now().minute, datetime.now().second, ctx.message.channel.name, ",".join(recipients_list))
+            intro = "Log started by {6} on {0:04d}/{1:02d}/{2:02d} at {3:02d}:{4:02d}:{5:02d} in channel {7} with recipients {8}.\nUser ID: {9}\nChannel ID: {10}\nChannel type: {11}\n".format(datetime.now().year, datetime.now().month, datetime.now().day, datetime.now().hour, datetime.now().minute, datetime.now().second, ctx.message.author.name, ctx.message.channel.name, ", ".join(recipients_list), ctx.message.author.id, ctx.message.channel.id, ctx.message.channel.type)
         else:
             filename = 'Server-{6}-{7}-{0:04d}{1:02d}{2:02d}-{3:02d}-{4:02d}-{5:02d}.log'.format(datetime.now().year, datetime.now().month, datetime.now().day, datetime.now().hour, datetime.now().minute, datetime.now().second, ctx.message.server.name, ctx.message.channel.name)
+            intro = "Log started by {6} on {0:04d}/{1:02d}/{2:02d} at {3:02d}:{4:02d}:{5:02d} in channel {7} on server {8}.\nUser ID: {9}\nChannel ID: {10}\nServer ID: {11}\nChannel type: {12}\n".format(datetime.now().year, datetime.now().month, datetime.now().day, datetime.now().hour, datetime.now().minute, datetime.now().second, str(ctx.message.author), str(ctx.message.channel), str(ctx.message.server), ctx.message.author.id, ctx.message.channel.id, ctx.message.server.id, ctx.message.channel.type)
 
         async for message in self.bot.logs_from(ctx.message.channel, messages):
             save_logs(filename, make_message(self, message) + "\n")
             #save_logs(filename, "[{0:04d}/{1:02d}/{2:02d}-{3:02d}:{4:02d}:{5:02d}] <{6}> {7}\n".format(message.timestamp.year, message.timestamp.month, message.timestamp.day, message.timestamp.hour, message.timestamp.minute, message.timestamp.second, message.author.name.encode('ascii', 'backslashreplace').decode('ascii'), message.content.encode('ascii', 'backslashreplace').decode('ascii')))
 
-        save_logs(filename, "Log started by {6} on {0:04d}/{1:02d}/{2:02d} at {3:02d}:{4:02d}:{5:02d} in channel {7} on server {8}.\nUser ID: {9}\nChannel ID: {10}\nServer ID: {11}\nChannel type: {12}\n".format(datetime.now().year, datetime.now().month, datetime.now().day, datetime.now().hour, datetime.now().minute, datetime.now().second, str(ctx.message.author), str(ctx.message.channel), str(ctx.message.server), ctx.message.author.id, ctx.message.channel.id, ctx.message.server.id, ctx.message.channel.type))
+        save_logs(filename, intro)
 
         with open("data/logger/invoked_logs/"+filename,"rb") as f_in, gzip.open("data/logger/invoked_logs/"+filename+'.gz', 'wb') as f_out:
             f_out.writelines(f_in)
@@ -220,19 +223,22 @@ class Logger:
             for recipient in ctx.message.channel.recipients:
                 recipients_list += "{}#{}".format(recipient.name, recipient.discriminator)
             filename = 'DM-({6})-{0:04d}{1:02d}{2:02d}-{3:02d}-{4:02d}-{5:02d}.log'.format(datetime.now().year, datetime.now().month, datetime.now().day, datetime.now().hour, datetime.now().minute, datetime.now().second, ",".join(recipients_list))
+            intro = "Log started by {6} on {0:04d}/{1:02d}/{2:02d} at {3:02d}:{4:02d}:{5:02d} with recipient {7}.\nUser ID: {8}\nChannel ID: {9}\nChannel type: {10}\n".format(datetime.now().year, datetime.now().month, datetime.now().day, datetime.now().hour, datetime.now().minute, datetime.now().second, ctx.message.author.name, ", ".join(recipients_list), ctx.message.author.id, ctx.message.channel.id, ctx.message.channel.type)
         elif (str(ctx.message.channel.type) == "group"):
             recipients_list = []
             for recipient in ctx.message.channel.recipients:
                 recipients_list += "{}#{}".format(recipient.name, recipient.discriminator)
             filename = 'Group-{6}-({7})-{0:04d}{1:02d}{2:02d}-{3:02d}-{4:02d}-{5:02d}.log'.format(datetime.now().year, datetime.now().month, datetime.now().day, datetime.now().hour, datetime.now().minute, datetime.now().second, ctx.message.channel.name, ",".join(recipients_list))
+            intro = "Log started by {6} on {0:04d}/{1:02d}/{2:02d} at {3:02d}:{4:02d}:{5:02d} in channel {7} with recipients {8}.\nUser ID: {9}\nChannel ID: {10}\nChannel type: {11}\n".format(datetime.now().year, datetime.now().month, datetime.now().day, datetime.now().hour, datetime.now().minute, datetime.now().second, ctx.message.author.name, ctx.message.channel.name, ", ".join(recipients_list), ctx.message.author.id, ctx.message.channel.id, ctx.message.channel.type)
         else:
             filename = 'Server-{6}-{7}-{0:04d}{1:02d}{2:02d}-{3:02d}-{4:02d}-{5:02d}.log'.format(datetime.now().year, datetime.now().month, datetime.now().day, datetime.now().hour, datetime.now().minute, datetime.now().second, ctx.message.server.name, ctx.message.channel.name)
+            intro = "Log started by {6} on {0:04d}/{1:02d}/{2:02d} at {3:02d}:{4:02d}:{5:02d} in channel {7} on server {8}.\nUser ID: {9}\nChannel ID: {10}\nServer ID: {11}\nChannel type: {12}\n".format(datetime.now().year, datetime.now().month, datetime.now().day, datetime.now().hour, datetime.now().minute, datetime.now().second, str(ctx.message.author), str(ctx.message.channel), str(ctx.message.server), ctx.message.author.id, ctx.message.channel.id, ctx.message.server.id, ctx.message.channel.type)
 
         log_content = ""
         async for message in self.bot.logs_from(ctx.message.channel, messages):
             log_content = make_message(self, message) + "\n" + log_content
 
-        save_logs(filename, "Log started by {6} on {0:04d}/{1:02d}/{2:02d} at {3:02d}:{4:02d}:{5:02d} in channel {7} on server {8}.\nUser ID: {9}\nChannel ID: {10}\nServer ID: {11}\nChannel type: {12}\n{13}".format(datetime.now().year, datetime.now().month, datetime.now().day, datetime.now().hour, datetime.now().minute, datetime.now().second, str(ctx.message.author), str(ctx.message.channel), str(ctx.message.server), ctx.message.author.id, ctx.message.channel.id, ctx.message.server.id, ctx.message.channel.type, log_content))
+        save_logs(filename, intro)
 
         with open("data/logger/invoked_logs/"+filename,"rb") as f_in, gzip.open("data/logger/invoked_logs/"+filename+'.gz', 'wb') as f_out:
             f_out.writelines(f_in)
